d1d03f082805215a88ebcc67d358958808d3c16b
Administrator <buiquangminh30102001@gmail.com>
2021-03-03 20:06:03 +0700

    update

---
+++ t2010e_nodejs/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();// xay nha
+const port = process.env.PORT || 5000; // khai bao cong vao
+app.listen(port,function () { // hoan thanh xay nha
+    console.log("Server is running..");
+});
+app.use(express.static("public"));// cac file css,image,.. phai dat trong public
+app.set("view engine","ejs");
+// tao routing ( thue 1 nhan vien)
+app.get("/",function (req,res) {
+    res.sendFile(__dirname+"./public/about.html");
+})
+// them 1 routing
+app.get("/bongda",function (req,res) {
+    var d = {
+        message:"success",
+        data: [
+            {
+                id:1,
+                name:"haha"
+
+            },
+            {
+                id:2,
+                name:"hihi"
+            }
+        ]
+    }
+    res.send(d);
+})
+// trang quan ao
+app.get("/quan-ao",function (req,res) {
+    res.sendFile(__dirname+"/public/Session7_bootstrap.html");
+})
+var count = 0;
+app.get("/dem-so-nguoi",function (req,res) {
+    count++;
+    res.render("demo",{
+        count:count
+    });
+})
---
+++ t2010e_nodejs/public/imgs/js/header.js
@@ -0,0 +1,17 @@
+var a = document.getElementById('hidden-nav');
+var b = document.getElementById('hidden-cart');
+function toggleNav() {
+    if( !a.style.width || a.style.width === '0%'){
+        a.style.width = '100%';
+    }else{
+        a.style.width = '0%';
+    }
+}
+
+function openCart() {
+    b.style.display = 'block';
+    a.style.width = '0%';
+}
+function closeCart() {
+    b.style.display = 'none';
+}
